55de914b450d670dae11a22a1640ddac36669806
Brandon Y Lam <me@brandonlam.ca>
2020-11-24 15:21:46 -0800

    add tests for #create

--- spec/controllers/job_posts_controller_spec.rb
+++ spec/controllers/job_posts_controller_spec.rb
@@ -30,4 +30,40 @@ RSpec.describe JobPostsController, type: :controller do
       # we check that the instance variable @job_post is a new instance of the class JobPost (Model)
     end
   end
+
+  describe "#create" do
+    def valid_request
+      post(:create, params: { job_post: FactoryBot.attributes_for(:job_post) })
+    end
+
+    it "creates a job post in the database" do
+      # GIVEN
+      count_before = JobPost.count # the number of all the records in the JobPost table
+      # WHEN
+      valid_request
+      # mocking a post request to the create method. The params of the request will look similar to: {
+      #   job_post: {
+      #     title: 'senior dev',
+      #     description: 'lots of pay',
+      #     location: 'remote',
+      #     min_salary: 500_000,
+      #     max_salary: 1_000_000
+      #   }
+      # }
+
+      # THEN
+      count_after = JobPost.count
+      expect(count_after).to(eq(count_before + 1))
+      # eq is an assertion provided by Rspec that checks the value to the right of the .to is equal to the paramater passed in to the method.
+    end
+
+    it "redirects us to the show page for that post" do
+      # GIVEN
+      # WHEN
+      valid_request
+      # THEN
+      job_post = JobPost.last
+      expect(response).to(redirect_to(job_post_url(job_post.id)))
+    end
+  end
 end
