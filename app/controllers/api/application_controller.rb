b3ef6c7baa2b9c6934547f7341ae66489855f45e
Naveed Raza <naveedraza@gmail.com>
2020-12-28 14:52:54 -0800

    Error handling at backend

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,3 +1,41 @@
 class Api::ApplicationController < ApplicationController
   skip_before_action :verify_authenticity_token
+rescue_from ActiveRecord:: RecordInvalid, with: :record_invalid
+
+private
+def record_invalid(error)
+
+# For a ActiveRecord::InvalidRecord error, the .record
+# method returns the 'model instance' that failed the validation
+#  and raised the error
+  invalid_record = error.record
+     # {
+        #   errors: [
+        #     {
+        #       type: "ActiveRecord::InvalidRecord",
+        #       record_type: "Question",
+        #       field: "body",
+        #       message: "..."
+        #     }
+        #   ]
+        # }
+
+
+
+  errors = invalid_record.errors.map do |field, message|
+    {
+      type: error.class.to_s,
+      record_type: invalid_record.class.to_s,
+      field:field,
+      message: message
+
+    }
+  end
+  render(
+    json: {status: 422, errors:errors},
+    status: 422
+  )
+
+end
+
 end
